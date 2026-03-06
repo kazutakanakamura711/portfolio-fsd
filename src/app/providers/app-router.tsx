da2074d65d9fa84d5c5a41129d05b48037a91611
kazutakanakamura711 <a711st777@gmail.com>
2026-03-06 15:22:00 +0900

    feat: add footer with copyright and tech stack

--- src/app/providers/app-router.tsx
+++ src/app/providers/app-router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, useRoutes } from 'react-router-dom'
 import { routes } from '@/app/routes'
 import { HeaderContainer } from '@/widgets/header'
+import { FooterContainer } from '@/widgets/footer'
 
 const AppRoutes = () => {
   const element = useRoutes(routes)
@@ -8,6 +9,7 @@ const AppRoutes = () => {
     <>
       <HeaderContainer />
       <main className="pt-14">{element}</main>
+      <FooterContainer />
     </>
   )
 }
